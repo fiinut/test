f274956256e69b95de71077f24395ed014073bc5
Fern <70007693@krungsri.com>
2022-07-21 11:31:44 +0700

    config

--- .nuxt/components/index.js
+++ .nuxt/components/index.js
@@ -1,3 +1,8 @@
+export { default as ModalAddLinkBannerModal } from '../../components/modal/AddLinkBannerModal.vue'
+export { default as ModalConfirm } from '../../components/modal/ModalConfirm.vue'
+export { default as ModalPreviewContentModal } from '../../components/modal/PreviewContentModal.vue'
+export { default as ModalSuccessModal } from '../../components/modal/SuccessModal.vue'
+export { default as ModalUploadImgModal } from '../../components/modal/UploadImgModal.vue'
 export { default as ElementsButton } from '../../components/elements/Button.vue'
 export { default as ElementsCheckboxField } from '../../components/elements/CheckboxField.vue'
 export { default as ElementsDatepicker } from '../../components/elements/Datepicker.vue'
@@ -6,11 +11,6 @@ export { default as ElementsRadioButton } from '../../components/elements/RadioB
 export { default as ElementsSelectField } from '../../components/elements/SelectField.vue'
 export { default as ElementsSwitchesField } from '../../components/elements/SwitchesField.vue'
 export { default as ElementsTable } from '../../components/elements/Table.vue'
-export { default as ModalAddLinkBannerModal } from '../../components/modal/AddLinkBannerModal.vue'
-export { default as ModalConfirm } from '../../components/modal/ModalConfirm.vue'
-export { default as ModalPreviewContentModal } from '../../components/modal/PreviewContentModal.vue'
-export { default as ModalSuccessModal } from '../../components/modal/SuccessModal.vue'
-export { default as ModalUploadImgModal } from '../../components/modal/UploadImgModal.vue'
 
 // nuxt/nuxt.js#8607
 function wrapFunctional(options) {
